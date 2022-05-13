26e0469c1ed8a6b8a5bda894a440adb5f8c5062c
Park JuHan <1004sunye@naver.com>
2022-05-13 20:31:32 +0900

    Update 완주하지못한선수(hash).cpp

--- hash/완주하지못한선수(hash).cpp
+++ hash/완주하지못한선수(hash).cpp
@@ -8,6 +8,12 @@ string solution(vector<string> participant, vector<string> completion) {
   string answer = ""; 
   unordered_map<string, int> map; 
   
+  /*
+	name.find("one")
+	키 값이 "one"인 원소의 iterator 리턴
+	찾는 값이 없다면 name.end() 리턴
+	*/
+  
   for (auto player : participant) { 
     if (map.end() == map.find(player)) 
       map.insert(make_pair(player, 1)); 
